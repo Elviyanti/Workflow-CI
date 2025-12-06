76463ab5f7e2d57660c27594b50070ece52b1b8a
Elviyanti <165618675+Elviyanti@users.noreply.github.com>
2025-12-06 20:35:15 +0700

    Create modelling.py

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load dataset hasil preprocessing
+df = pd.read_csv("heartdataset_preprocessing.csv")
+
+# Pisahkan fitur dan target
+X = df.drop("target", axis=1)
+y = df["target"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Aktifkan autolog MLflow
+mlflow.autolog()
+
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+    
+    print(f"Accuracy: {acc}")
